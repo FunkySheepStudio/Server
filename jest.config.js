1463dab1ed0636457f0af66e6c085232678ddb2b
BartKnucle <sjarde@yahoo.fr>
2021-06-01 19:31:50 +0200

    Tests, game update and connection ui fix

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,9 @@ module.exports = {
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/server/**/*.js',
+    '<rootDir>/client/store/**/*.js',
+    '<rootDir>/client/lib/**/*.js',
+    '<rootDir>/client/**/*.vue'
   ]
 }
--- server/services/management/users/class.js
+++ server/services/management/users/class.js
@@ -12,6 +12,7 @@ exports.Users = class Users extends ServiceClass {
   //  On user connection
   onConnect (authResult) {
     this.app.service('/api/management/connections').create({
+      _id: authResult.accessToken,
       startedAt: Date.now(),
       user: authResult.user._id,
       type: 'web'
