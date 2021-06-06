aeba4bd661e1a43247aa75289f2129b527c8f1b8
BartKnucle <sjarde@yahoo.fr>
2021-06-06 19:28:20 +0200

    Start homepage

---
+++ client/components/users/online.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <v-progress-circular
+      :size="100"
+    >
+      {{ users().total }}
+    </v-progress-circular>
+  </section>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters('users', { users: 'find' })
+  },
+  mounted () {
+    this.findUsers()
+  },
+  methods: {
+    ...mapActions('users', { findUsers: 'find' })
+  }
+}
+</script>
+<style>
+</style>
