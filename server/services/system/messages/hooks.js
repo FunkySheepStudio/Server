0fd15e99e2402bca429f05824724b5dffc61e4f7
BartKnucle <sjarde@yahoo.fr>
2021-11-15 13:31:18 +0100

    Add error catching in messages

--- server/services/system/messages/hooks.js
+++ server/services/system/messages/hooks.js
@@ -15,21 +15,39 @@ function dispatch (context) {
   switch (context.data.request) {
     case 'find':
       context.app.service(context.data.service).find(context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     case 'get':
       context.app.service(context.data.service).get(context.data.key, context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     case 'create':
       context.app.service(context.data.service).create(context.data.data, context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     case 'update':
       context.app.service(context.data.service).update(context.data.key, context.data.data, context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     case 'patch':
       context.app.service(context.data.service).patch(context.data.key, context.data.data, context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     case 'remove':
       context.app.service(context.data.service).remove(context.data.key, context.data.params)
+        .catch((err) => {
+          console.log(err)
+        })
       break
     default:
       break
