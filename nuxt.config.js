59e429356800952cc2f8466172c0225cd8bf81f3
BartKnucle <sjarde@yahoo.fr>
2022-02-11 16:09:20 +0100

    Update version

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,6 +75,19 @@ module.exports = {
       if (ctx.isDev) {
         config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
       }
-    }
+    },
+    babel: {
+      presets({isServer}) {
+        const targets = isServer ? { node: 'current' } : { ie: 11 }
+        return [
+          [ require.resolve("@babel/preset-env"), { targets }  ]
+        ]
+      },
+      plugins: [
+        "@babel/syntax-dynamic-import",
+        "@babel/transform-runtime",
+        "@babel/transform-async-to-generator"
+      ]
+    }  
   }
 }
